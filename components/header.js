b4a0c8c0635960df42c8c17be762e2c5caacdd50
George499 <53001250+George499@users.noreply.github.com>
2021-03-16 19:10:22 +0300

    header

--- components/header.js
+++ components/header.js
@@ -21,34 +21,72 @@ const Header = ({ navigation }) => {
   });
 
   return (
-    <header>
-      <nav
-        className={`navbar navbar-light ${isBlack ? "bg-dark" : "bg-light"}`}
-      >
-        <div className="container-fluid">
-          <Link href="/">
-            <a>
-              <img
-                src="https://cdn.worldvectorlogo.com/logos/next-js.svg"
-                className="navbar-brand"
-                width="50"
-                height="44"
-                alt="..."
-              />
-            </a>
-          </Link>
-          <ul className="list-group flex-row list-unstyled flex justify-content-end">
-            {navigation.map((item) => (
-              <li className="nav-item" key={item.id}>
-                <Link href={`${item.Slug}`}>
-                  <a className="nav-link">{item.Title}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
+    <header className="header header-absolute">
+      <div className="main_bg-color ">
+        <nav className="bg-hover-white bg-fixed-white navbar-hover-light navbar-fixed-light navbar navbar-expand-lg  bg-transparent">
+          <div class="container-fluid">
+            <Link href="/">
+              <a>
+                <img
+                  src="https://cdn.worldvectorlogo.com/logos/next-js.svg"
+                  className="navbar-brand"
+                  width="50"
+                  height="44"
+                  alt="..."
+                />
+              </a>
+            </Link>
+            <button
+              aria-label="Toggle navigation"
+              type="button"
+              className="navbar-toggler-right navbar-toggler"
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+            <div className="collapse navbar-collapse">
+              <ul className="mx-auto navbar-nav">
+                {navigation.map((item) => (
+                  <li className=" nav-item" key={item.id}>
+                    <Link href={`${item.Slug}`}>
+                      <a className="nav-link main_text-color ">{item.Title}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
+
+    // <header>
+    //   <nav
+    //     className={`navbar navbar-light ${isBlack ? "bg-dark" : "bg-light"}`}
+    //   >
+    //     <div className="container-fluid">
+    //       <Link href="/">
+    //         <a>
+    //           <img
+    //             src="https://cdn.worldvectorlogo.com/logos/next-js.svg"
+    //             className="navbar-brand"
+    //             width="50"
+    //             height="44"
+    //             alt="..."
+    //           />
+    //         </a>
+    //       </Link>
+    //       <ul className="list-group flex-row list-unstyled flex justify-content-end">
+    //         {navigation.map((item) => (
+    //           <li className="nav-item" key={item.id}>
+    //             <Link href={`${item.Slug}`}>
+    //               <a className="nav-link">{item.Title}</a>
+    //             </Link>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   </nav>
+    // </header>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/styles.css";
+import "../styles/styles.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/header";
 import { DefaultSeo } from "next-seo";
