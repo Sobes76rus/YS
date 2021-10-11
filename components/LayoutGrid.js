fc7541ae0eb7028533a404d0669dd46228ee3e87
George499 <53001250+George499@users.noreply.github.com>
2021-10-11 16:15:01 +0300

    2nd try

--- components/LayoutGrid.js
+++ components/LayoutGrid.js
@@ -1,5 +1,6 @@
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import Product from "./Product";
+import { Container, Row, Col } from "reactstrap";
 import useWindowSize from "../hooks/useWindowSize";
 
 export default function LayoutGrid({ cards }) {
@@ -7,21 +8,36 @@ export default function LayoutGrid({ cards }) {
   const isSlim = windowSize.width <= "1200";
 
   return (
-    <ResponsiveMasonry
-      style={{ marginTop: "50px" }}
-      columnsCountBreakPoints={{ 300: 2, 900: 3, 1100: 4 }}
-    >
-      <Masonry gutter="30px">
+    // <ResponsiveMasonry
+    //   style={{ marginTop: "50px" }}
+    //   columnsCountBreakPoints={{ 300: 2, 900: 3, 1100: 4 }}
+    // >
+    //   <Masonry gutter="30px">
+    //     {cards.map((value) => (
+    //       <div key={value.id} style={{ marginTop: "-30px" }}>
+    //         {isSlim ? (
+    //           <Product card={value} masonry onlyViewButton />
+    //         ) : (
+    //           <Product card={value} masonry />
+    //         )}
+    //       </div>
+    //     ))}
+    //   </Masonry>
+    // </ResponsiveMasonry>
+    <Container fluid style={{ marginTop: "50px" }}>
+      <Row xs="4">
         {cards.map((value) => (
-          <div key={value.id} style={{ marginTop: "-30px" }}>
-            {isSlim ? (
-              <Product card={value} masonry onlyViewButton />
-            ) : (
-              <Product card={value} masonry />
-            )}
-          </div>
+          <Col key={value.id} style={{ alignSelf: "center" }}>
+            <div style={{ marginTop: "-30px" }}>
+              {isSlim ? (
+                <Product card={value} onlyViewButton />
+              ) : (
+                <Product card={value} />
+              )}
+            </div>
+          </Col>
         ))}
-      </Masonry>
-    </ResponsiveMasonry>
+      </Row>
+    </Container>
   );
 }
