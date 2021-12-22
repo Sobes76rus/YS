070c3ef2824f8a97485e28673af8ade994f3aa84
Anton <myprojectorterrypratchett@gmail.com>
2021-12-22 15:29:51 +0300

    fix: onLoad не работает на скрипты

--- pages/_app.js
+++ pages/_app.js
@@ -43,6 +43,8 @@ function MyApp({ Component, pageProps }) {
   };
 
   useEffect(() => {
+    initYM();
+
     const handleStart = () => {
       setIsLoading(true);
       NProgress.start();
@@ -77,11 +79,7 @@ function MyApp({ Component, pageProps }) {
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-       
   `,
-        }}
-        onLoad={() => {
-          initYM();
         }}
       />
       <DefaultSeo {...SEO} />
