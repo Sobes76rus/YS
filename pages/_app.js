3472cb43fa1b10e64344d015f8574042471b605e
Anton <myprojectorterrypratchett@gmail.com>
2021-12-22 11:06:31 +0300

    fix: Инициализируем, если нет счетчика

--- pages/_app.js
+++ pages/_app.js
@@ -15,13 +15,31 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
-  const [isYMCalled, setIsYMCalled] = useState(false);
   NProgress.configure({ showSpinner: false });
 
+  const code = 55422358;
+  const key = `yaCounter${code}`;
   const handleYM = () => {
-    const code = 55422358;
-    setIsYMCalled(true);
-    window[`yaCounter${code}`].hit(window.location.pathname);
+    if (window[key]) {
+      window[key].hit(window.location.pathname);
+    }
+  };
+  const initYM = () => {
+    if (!window[key]) {
+      window.ym(code, "init", {
+        defer: true,
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        // Отключить вебвизор по просьбе Витали, т.к. не нужно
+        // webvisor:true,
+        trackHash: true,
+      });
+
+      // После инициализации тоже необходимо уведомить ЯМ
+      // т.к. мы отключили автоматическое уведомление флагом defer
+      handleYM();
+    }
   };
 
   useEffect(() => {
@@ -53,28 +71,17 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         type="text/javascript"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
        
-          ym(55422358, "init", {
-               defer:true,
-               clickmap:true,
-               trackLinks:true,
-               accurateTrackBounce:true,
-               // Отключить вебвизор по просьбе Витали, т.к. не нужно
-               // webvisor:true,
-               trackHash:true
-          });
   `,
         }}
         onLoad={() => {
-          // После инициализации тоже необходимо уведомить ЯМ
-          // т.к. мы отключили автоматическое уведомление флагом defer
-          if (!isYMCalled) handleYM();
+          initYM();
         }}
       />
       <DefaultSeo {...SEO} />
