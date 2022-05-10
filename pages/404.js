0fff2ddc4b78e7697e374410ca15f4a8f6078519
Anton <myprojectorterrypratchett@gmail.com>
2022-05-11 01:27:02 +0300

    fix: добавляем ревалидацию статик страниц

--- pages/404.js
+++ pages/404.js
@@ -35,6 +35,8 @@ export async function getStaticProps() {
       headerAbsolute: true,
       title: "404",
     },
+
+    revalidate: 60,
   };
 }
 
--- pages/contacts.js
+++ pages/contacts.js
@@ -55,6 +55,8 @@ export async function getStaticProps() {
         ],
       },
     },
+
+    revalidate: 60,
   };
 }
 
